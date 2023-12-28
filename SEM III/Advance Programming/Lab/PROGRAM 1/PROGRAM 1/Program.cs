4a0c8653faee355de21e81ba15d47e83edc1d3f8
Chiranjeevi M N <135362312+chiranjeevimn@users.noreply.github.com>
2023-12-28 20:52:59 +0530

    Program 1

---
+++ SEM III/Advance Programming/Lab/PROGRAM 1/PROGRAM 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PROGRAM_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The number of arguments are: {0}", args.Length);
+            for (int i = 0; i < args.Length; i++)
+                Console.WriteLine("argument {0} is {1} ", i + 1, args[i]);
+            Console.ReadLine();
+        }
+    }
+}
