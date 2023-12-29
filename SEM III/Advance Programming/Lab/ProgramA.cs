41dbb834a2d0c06738ebf4537cfeb397aef4c6e1
Chiranjeevi M N <135362312+chiranjeevimn@users.noreply.github.com>
2023-12-29 10:25:05 +0530

    Prg 3A ZeroDivisionError

---
+++ SEM III/Advance Programming/Lab/ProgramA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Program_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Numerator");
+            int p = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the numerator");
+            int q = int.Parse(Console.ReadLine());
+            try
+            {
+                int r = p / q;
+                Console.WriteLine("The value of quotient is {0} ", r);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
