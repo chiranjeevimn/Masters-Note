5c82fd8c1b99e9bcece993982b5c3835ddc21fab
Chiranjeevi M N <135362312+chiranjeevimn@users.noreply.github.com>
2024-01-01 09:33:46 +0530

    Override and Virtual keyword

---
+++ SEM III/Advance Programming/Lab/Program 4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Program_4
+{
+    class baseclass
+    {
+        public virtual string city()
+        {
+            return "BMSCE";
+        }
+    }
+    class Derivedclass:baseclass
+    {
+        public override string city()
+        {
+            return "BMSIT";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Derivedclass o = new Derivedclass();
+            string str = o.city();
+            Console.WriteLine("College name is {0}", str);
+            Console.ReadKey();
+        }
+    }
+}
