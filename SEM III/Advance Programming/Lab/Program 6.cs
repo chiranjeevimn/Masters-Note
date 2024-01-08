929961d9e19e24e5ced9bc9959a1cf17dbcccecf
Chiranjeevi M N <135362312+chiranjeevimn@users.noreply.github.com>
2024-01-08 09:25:00 +0530

    Abstract class and Method

---
+++ SEM III/Advance Programming/Lab/Program 6.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Program_6
+{
+    public abstract class Shape
+    {
+        public abstract void draw();
+    }
+    public class Rectangle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing rectangle...");
+        }
+    }
+    public class Circle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing circle...");
+        }
+    }
+    public class TestAbstract
+    {
+        public static void Main()
+        {
+            Shape s;
+            s = new Rectangle();
+            s.draw();
+            s = new Circle();
+            s.draw();
+            Console.ReadKey();
+        }
+    }
+}
